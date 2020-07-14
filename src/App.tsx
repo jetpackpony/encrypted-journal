ebc27f5fdbe1f87331126384a8997bc895d141d6
jetpackpony <jetpackpony@gmail.com>
2020-07-14 17:20:02 +0300

    Add key derivation on startup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import { h } from 'preact';
+import { RecoilRoot, useRecoilState } from 'recoil';
 import Editor from './components/Editor';
+import { cryptoKeyState } from './store';
+import { deriveKey, getRandomBytes } from './crypto';
 
-const App = () => <Editor/>;
+const App = () => {
+  const [key, setCryptoKey] = useRecoilState(cryptoKeyState);
+  if (!key) {
+    const salt = getRandomBytes();
+    deriveKey("1234", salt)
+      .then((res) => {
+        setCryptoKey(res);
+      });
+  }
+  return (key) ? <Editor /> : "Loading...";
+};
 
 export default App;
