e10d4e8f75e4da60e9d0bd914001c765c9069417
jetpackpony <jetpackpony@gmail.com>
2020-07-14 17:20:33 +0300

    Add a simple encrypt-decrypt of text

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,18 +1,25 @@
 import { h } from 'preact';
 import { useState } from 'preact/hooks';
+import { useRecoilValue } from 'recoil';
+import { cryptoKeyState } from '../store';
+import { encrypt, decrypt } from '../crypto';
 
 const Editor = () => {
+  const cryptoKey = useRecoilValue(cryptoKeyState);
   const [text, setText] = useState("");
   const updateText = (e) => {
     setText(e.target.value);
   };
-  const run = (e) => {
+  const run = async (e) => {
     e.preventDefault();
-    console.log("Text: ", text);
+    console.log("Initial Text: ", text);
+    const [counter, ciphertext] = await encrypt(cryptoKey, text);
+    const decrypted = await decrypt(cryptoKey, counter, ciphertext);
+    console.log("Enc/dec text: ", decrypted);
   };
   return (
     <div>
-      <textarea style={{ width:"600px", height:"700px"}} onInput={updateText}>
+      <textarea style={{ width:"600px", height:"700px"}} onChange={updateText}>
         {text}
       </textarea>
       <button onClick={run}>Run</button>
